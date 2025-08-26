0ee4b71f88fb7ef7fbdbc58ee15723792fce3a46
권경민 <phillip0919@gmail.com>
2025-08-26 14:29:20 +0900

    feat: CounterApp Ui

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import Controller from "./components/Controller";
+import Viewer from "./components/Viewer";
 
 function App() {
-  return <>counterApp</>;
+  return (
+    <div className="app">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
+  );
 }
 
 export default App;
